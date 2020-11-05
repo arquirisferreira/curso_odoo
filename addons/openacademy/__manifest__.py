4bc0a1134eb52776da8121179db7054db708e8e5
arquirisferreira <arquiris@myia.com.br>
2020-11-05 21:40:56 +0000

    Alteração do nome do Módulo

--- addons/openacademy/__manifest__.py
+++ addons/openacademy/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "openacademy",
+    'name': "Open Academy",
 
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
@@ -17,7 +17,7 @@
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Estudo',
-    'version': '0.1',
+    'version': '0.2',
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
